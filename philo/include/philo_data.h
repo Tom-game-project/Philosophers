ef01bc4a154dec32b29451fb2af076e28c730f6d
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2025-04-05 02:29:58 +0900

    [WIP] includeディレクトリを作成した

---
+++ philo/include/philo_data.h
@@ -0,0 +1,38 @@
+#ifndef PHILO_FORK_H
+#define PHILO_FORK_H
+
+#include <pthread.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+/// 引数の情報を格納する
+typedef struct s_info_table t_info_table;
+typedef struct s_philo_fork t_philo_fork;
+
+/// 以下の構造体はすべての哲学者が共有する
+struct s_info_table
+{
+	// 引数で与えられる条件
+	int number_of_philosophers;
+	useconds_t time_to_die;
+	useconds_t time_to_eat;
+	useconds_t time_to_sleep;
+	int number_of_times_each_philosopher_must_eat;
+	bool die_flag; // 最初はfalse、哲学者が死んだタイミングでtrueになる
+};
+
+typedef struct s_philosopher_data t_philosopher_data;
+struct s_philosopher_data{
+	int philo_id;  // スレッドごとの識別番号
+	t_info_table *info; // 引数で与えられた条件と、他の哲学者が死んだかどうかの情報を保持する構造体
+	t_philo_fork *r_fork; // 右手に持てるフォーク
+	t_philo_fork *l_fork; // 左手に持てるフォーク
+} ;
+
+struct s_philo_fork{
+	pthread_mutex_t *mutex; // この構造体のデータは
+				// mutexで保護されている場合だけ触れる
+	int	fork_id;
+};
+
+#endif
--- philo/src/philo.c
+++ philo/src/philo.c
@@ -3,38 +3,9 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include "philo_data.h"
 
 void *thread_func(void *param);
-
-/// 引数の情報を格納する
-typedef struct s_info_table t_info_table;
-typedef struct s_philo_fork t_philo_fork;
-
-/// 以下の構造体はすべての哲学者が共有する
-struct s_info_table
-{
-	// 引数で与えられる条件
-	int number_of_philosophers;
-	useconds_t time_to_die;
-	useconds_t time_to_eat;
-	useconds_t time_to_sleep;
-	int number_of_times_each_philosopher_must_eat;
-	bool die_flag; // 最初はfalse、哲学者が死んだタイミングでtrueになる
-};
-
-typedef struct s_philosopher_data t_philosopher_data;
-struct s_philosopher_data{
-	int philo_id;  // スレッドごとの識別番号
-	t_info_table *info; // 引数で与えられた条件と、他の哲学者が死んだかどうかの情報を保持する構造体
-	t_philo_fork *r_fork; // 右手に持てるフォーク
-	t_philo_fork *l_fork; // 左手に持てるフォーク
-} ;
-
-struct s_philo_fork{
-	pthread_mutex_t *mutex; // この構造体のデータは
-				// mutexで保護されている場合だけ触れる
-	int	fork_id;
-};
 
 /// 哲学者たちのフォークを用意する
 int set_forks(t_philo_fork f[], int number_of_philosophers)
@@ -59,6 +30,9 @@ int set_philos(
 {
 	int i;
 
+	(void) philos;
+	(void) forks;
+
 	i = 0;
 	while (i < number_of_philosophers)
 	{
@@ -75,6 +49,10 @@ int main(int argc, char *args[])
 	pthread_t *tid_table;
 	t_philosopher_data *philosophers;
 	t_philo_fork *forks;
+
+	(void) t;
+	(void) philosophers;
+	(void) forks;
 
 	/// 後で、引数を確かめる
 	t.number_of_philosophers = 5;
